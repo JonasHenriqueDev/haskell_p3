762b8967b9be2ca71744ff136e00d406059b590d
Jonas Santos <jonashssantos@gmail.com>
2023-06-02 00:14:24 -0300

    mais exercicios

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,7 +4,10 @@ module Lib
     converteCF,
     par,
     par2,
-
+    menor,
+    menor3,
+    fatorial,
+    fib,
   )
 where
 
@@ -27,3 +30,22 @@ par = even -- jeito mais fácil e curto --
 
 par2:: Int -> Bool
 par2 x = if mod x 2 == 0 then True else False -- jeito mais explicito --
+
+-- Faça uma função que recebe dois valores e retorna o menor. --
+menor :: Int -> Int -> Int
+menor x y = if x <= y then x else y
+
+-- Faça uma função que recebe três valores e retorna o menor. --
+menor3 :: Int -> Int -> Int -> Int
+menor3 x y z = if menor x y <= z then menor x y else z
+
+-- Escreva uma função recursiva para calcular o fatorial de um numero natural. --
+fatorial :: Int -> Int
+fatorial 0 = 1
+fatorial x = x * fatorial (x-1)
+
+-- Fibonacci --
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib x = fib (x-1) + fib (x-2)
