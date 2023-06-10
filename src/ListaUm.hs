fd181b4b333151bf47c9f8a14a1df1f94e77945b
Jonas Santos <jonashenrique965@gmail.com>
2023-06-10 16:36:30 -0300

    respondido até a Q6

--- src/ListaUm.hs
+++ src/ListaUm.hs
@@ -1,10 +1,17 @@
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
+
 module ListaUm
-    (   
+    (
         quantosIguais,
         mediaMaiores,
         potencia_2,
         potencia_4,
+        ouExclusivo,
+        x_maior,
+        x_menor,
+
     )
 where
 
@@ -19,7 +26,7 @@ quantosIguais a b c
 mediaMaiores :: (Ord a, Fractional a) => a -> a -> a -> Int
 mediaMaiores a b c =
     let media = (a + b + c)/3
-        maiores = length(filter (>media) [a, b, c]) in maiores
+        maiores = length (filter (>media) [a, b, c]) in maiores
 
 --Q3
 potencia_2 :: Int -> Int
@@ -28,3 +35,19 @@ potencia_2 x = x * x
 --Q4
 potencia_4 :: Int -> Int
 potencia_4 x = potencia_2 (potencia_2 x)
+
+--Q5
+ouExclusivo :: Bool -> Bool -> Bool
+ouExclusivo False False = False
+ouExclusivo False True = True
+ouExclusivo True False = True
+ouExclusivo True True = False
+
+--Q6
+x_maior :: Float -> Float -> Float -> Float
+x_maior a b c = (-b + sqrt (b * b  - 4 * a * c)) / (2 * a)
+
+x_menor :: Float -> Float -> Float -> Float
+x_menor a b c = (-b - sqrt (b * b  - 4 * a * c)) / (2 * a)
+
+--Q7
