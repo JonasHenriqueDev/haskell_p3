d24f913673da664a40d845b9c014fbfc02cdca00
Jonas Santos <jonashenrique965@gmail.com>
2023-06-10 16:09:02 -0300

    exercicios

--- src/ListaUm.hs
+++ src/ListaUm.hs
@@ -1,18 +1,30 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module ListaUm
     (   
-        media3,
-        questao1,
-        questao2,
+        quantosIguais,
+        mediaMaiores,
+        potencia_2,
+        potencia_4,
     )
 where
 
-media3 :: Int -> Int -> Int
-media3 a b c = (a + b + c) / 3
-
-questao1 :: Eq a => a -> a -> a -> Int
-questao1 a b c
+--Q1
+quantosIguais :: Eq a => a -> a -> a -> Int
+quantosIguais a b c
     | a == b && b == c = 3
     | a == b || a == c || b == c = 2
     | a /= b && b /= c = 0
 
-questao2 :: 
+--Q2
+mediaMaiores :: (Ord a, Fractional a) => a -> a -> a -> Int
+mediaMaiores a b c =
+    let media = (a + b + c)/3
+        maiores = length(filter (>media) [a, b, c]) in maiores
+
+--Q3
+potencia_2 :: Int -> Int
+potencia_2 x = x * x
+
+--Q4
+potencia_4 :: Int -> Int
+potencia_4 x = potencia_2 (potencia_2 x)
